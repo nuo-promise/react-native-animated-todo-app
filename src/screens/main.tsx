f5e7202e85a8941fc8510e48b41ef49d1a2a3767
nuo-promise <nuoyan@apache.org>
2021-11-29 00:39:00 +0800

    animated check box and label complete

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,13 +1,21 @@
-import * as React from 'react'
-import { Text, Box, Center, VStack, themeTools, useTheme, useColorMode, useColorModeValue } from 'native-base'
+import React, { useCallback, useState } from 'react'
+import { Pressable } from 'react-native'
+import { Text, Box, Center, VStack, useColorModeValue } from 'native-base'
 import ThemeToggle from '../components/theme-toggle'
+import AnimatedCheckbox from '../components/animated-checkbox'
+import TaskItem from '../components/task-item'
+
 export default function MainScreen() {
+
+  const [checked, setChecked] = useState(false)
+  const handlePressCheckbox = useCallback(() => {
+    setChecked(prev => !prev)
+  }, [])
+
   return (
     <Center _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} px={4} flex={1}>
       <VStack space={5} alignItems="center">
-        <Box>
-          <Text>Hello</Text>
-        </Box>
+        <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckbox} />
         <ThemeToggle />
       </VStack>
     </Center>
